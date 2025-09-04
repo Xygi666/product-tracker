29d72f1e8de502d381a27e2d0e7584c6b3d30cea
Xygi666 <91006842+Xygi666@users.noreply.github.com>
2025-09-04 16:55:47 +0300

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'product-cache-v1';
+const CACHE_NAME = 'product-tracker-v3.1';
 const urlsToCache = [
   './',
   './index.html',
@@ -13,46 +13,86 @@ const urlsToCache = [
   './js/search.js',
   './js/themes.js',
   './js/quick-add.js',
-  './icons/android-launcher-icon-192.png',
-  './icons/android-launcher-icon-512.png',
+  './icons/android-icon-192.png',
+  './icons/android-icon-512.png',
   './manifest.json'
 ];
 
+// Install event
 self.addEventListener('install', event => {
+  console.log('Service Worker: Installing...');
+  
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Service Worker: Caching resources...');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        console.log('Service Worker: All resources cached');
+        return self.skipWaiting();
+      })
+      .catch(err => {
+        console.error('Service Worker: Caching error:', err);
+      })
   );
 });
 
+// Activate event
 self.addEventListener('activate', event => {
+  console.log('Service Worker: Activating...');
+  
   event.waitUntil(
-    caches.keys().then(keys => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
       );
+    }).then(() => {
+      console.log('Service Worker: Activated');
+      return self.clients.claim();
     })
   );
 });
 
+// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request).then(response => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+    caches.match(event.request)
+      .then(response => {
+        // Return from cache if found
+        if (response) {
           return response;
         }
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      }).catch(() => {
-        if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-      });
-    })
+
+        // Otherwise make network request
+        return fetch(event.request)
+          .then(response => {
+            // Check if valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone response for cache
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(() => {
+            // If offline and requesting HTML page, return index
+            if (event.request.destination === 'document') {
+              return caches.match('./index.html');
+            }
+          });
+      })
   );
 });
